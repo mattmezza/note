7297d89cb4899f562adba2c9600f86b040716b15
Matteo Merola <mattmezza@gmail.com>
2025-02-03 18:43:01 +0100

    added extra perks to last cmd

--- note.sh
+++ note.sh
@@ -1,57 +1,72 @@
 function note() {
-    NOTE_DIR="$HOME"
-    if [ -d "${NOTE_SCRATCH_DIR}" ]; then
-        NOTE_DIR="${NOTE_SCRATCH_DIR}"
-    fi
+	NOTE_DIR="$HOME"
+	if [ -d "${NOTE_SCRATCH_DIR}" ]; then
+		NOTE_DIR="${NOTE_SCRATCH_DIR}"
+	fi
 
-    case "$1" in
-        list|l)
-            ls -1 $NOTE_DIR/*.txt
-            ;;
-        new|n)
-            FILE_NAME="$(date +%Y%m%d%H%M)-$(echo $2 | sed 's/ /_/g')"
-            $VISUAL "$NOTE_DIR/$FILE_NAME.txt"
-            ;;
-        open|o)
-            case "$2" in
-                last)
-                    $VISUAL $(note last)
+	case "$1" in
+		list|l|li)
+			ls -1 $NOTE_DIR/*.txt
+			;;
+		new|n|create|c)
+			FILE_NAME="$(date +%Y%m%d%H%M)-$(echo $2 | sed 's/ /_/g')"
+			$VISUAL "$NOTE_DIR/$FILE_NAME.txt"
+			;;
+		open|o|edit|e)
+			case "$2" in
+				last)
+					$VISUAL $(note last)
+					;;
+                last-*)
+                    idx="${1##*-}"
+                    tidx=$((idx+1))
+                    $VISUAL $(note last-$tidx)
                     ;;
-                *)
-                    $VISUAL "$NOTE_DIR/$2"
+				*)
+					$VISUAL "$NOTE_DIR/$2"
+					;;
+			esac
+			;;
+		path|p)
+			echo "$NOTE_DIR/$2"
+			;;
+		rm|delete)
+			case "$2" in
+				last)
+					rm -i $(note last)
+					;;
+                last-*)
+                    idx="${1##*-}"
+                    tidx=$((idx+1))
+                    rm -i $(note last-$tidx)
                     ;;
-            esac
+				*)
+					rm -i "$NOTE_DIR/$2"
+					;;
+			esac
+			;;
+		last)
+			echo $(note list | tail -n 1)
+			;;
+        last-*)
+            idx="${1##*-}"
+            tidx=$((idx+1))
+            echo $(note list | tail -n $tidx | head -n 1)
             ;;
-        path|p)
-            echo "$NOTE_DIR/$2"
-            ;;
-        rm)
-            case "$2" in
-                last)
-                    rm -i $(note last)
-                    ;;
-                *)
-                    rm -i "$NOTE_DIR/$2"
-                    ;;
-            esac
-            ;;
-        last)
-            echo $(note list | tail -n 1)
-            ;;
-        help)
-            echo "Usage:"
-            echo "  $ $0 list|l"
-            echo "  $ $0 new|n name"
-            echo "  $ $0 open|o name"
-            echo "  $ $0 path|p"
-            echo "  $ $0 path|p name"
-            echo "  $ $0 rm name"
-            echo "  $ $0 last"
-            echo "Your scratch notes dir is: '$NOTE_DIR'"
-            ;;
-        *)
-            echo "Missing command.\n$ $0 help"
-            return 1
-            ;;
-    esac
+		help|h)
+			echo "Usage:"
+			echo "  $ $0 list|l"
+			echo "  $ $0 new|n name"
+			echo "  $ $0 open|o name"
+			echo "  $ $0 path|p"
+			echo "  $ $0 path|p name"
+			echo "  $ $0 rm name"
+			echo "  $ $0 last"
+			echo "Your scratch notes dir is: '$NOTE_DIR'"
+			;;
+		*)
+			echo "Missing command.\n$ $0 help"
+			return 1
+			;;
+	esac
 }
